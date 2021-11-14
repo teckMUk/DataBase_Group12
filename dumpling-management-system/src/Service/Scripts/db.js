c10c913f8bffb5fe44e55f9e867878593eccbc3f
MukRaza <Mukeet@tcig.co>
2021-11-15 00:29:52 +0500

    Added Account and Empolyee

--- dumpling-management-system/src/Service/Scripts/db.js
+++ dumpling-management-system/src/Service/Scripts/db.js
@@ -11,12 +11,60 @@ var connectionString = mysql.createConnection(
         password:'Emaan@123',
     }
 );
+function createTable(q)
+{
+    connectionString.query(q,
+        (err,result)=>
+        {
+            if(err)
+            {
+                console.log("Table creation failed");
+                console.log(err);
+            }
+            else
+            {
+                console.log("Table created");
+                //console.log(result);
+            }
+        })
+}
+const createAccount = `CREATE TABLE IF NOT EXISTS dumpling.account(
+    accountId INT NOT NULL,
+    username VARCHAR(45) NOT NULL,
+    accountType VARCHAR(45) NOT NULL,
+    currentPassword VARCHAR(50) NOT NULL,
+    previousPassword VARCHAR(50) NOT NULL,
+    emailAddress VARCHAR(320) NOT NULL,
+    securityQuestions VARCHAR(2000) NOT NULL,
+    createdAt DATETIME NOT NULL,
+    updatedAt DATETIME NOT NULL,
+    archived BIT(1) NOT NULL DEFAULT 0,
+    PRIMARY KEY (accountId));`;
+    const createEmpolyee = `CREATE TABLE IF NOT EXISTS dumpling.employee(
+        employeeId INT NOT NULL,
+        employeeName VARCHAR(100) NOT NULL,
+        dateOfBirth DATE NOT NULL,
+        phoneNumber VARCHAR(15) NOT NULL,
+        address VARCHAR(100) NOT NULL,
+        position VARCHAR(45) NOT NULL,
+        salary INT NOT NULL,
+        bankAccountNumber BIGINT(20) NOT NULL,
+        createdAt DATETIME(1) NOT NULL,
+        updatedAt DATETIME(1) NOT NULL,
+        archived BIT(1) NOT NULL DEFAULT 0,
+        accountId INT NOT NULL,
+        PRIMARY KEY (employeeId),
+        CONSTRAINT accountId
+            FOREIGN KEY (accountId)
+            REFERENCES dumpling.account (accountId)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE);`;
 connectionString.connect((error)=>
 {
     if(!error)
     {
         console.log("Connection has been established");
-        connectionString.query("CREATE DATABASE dumpling", (err2,result) =>
+        connectionString.query("CREATE DATABASE IF NOT EXISTS dumpling", (err2,result) =>
         {
             if(err2)
             {
@@ -24,7 +72,10 @@ connectionString.connect((error)=>
             }
             else{
                 console.log("Database Created");
-                console.log(result);
+                createTable(createAccount);
+                createTable(createEmpolyee);
+                connectionString.end();
+                //console.log(result);
             }
         });
     }
@@ -34,9 +85,3 @@ connectionString.connect((error)=>
         console.log(error);
     }
 });
-const port = process.env.PORT || 3000;
-app.listen(port,()=>console.log(`Listening on port ${port}`));
-
-
-
-

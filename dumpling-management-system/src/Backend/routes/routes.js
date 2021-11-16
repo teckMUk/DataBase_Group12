08a89665cc8b0ae67a8c2ff40330c9ad4fa1e2c9
Naima02 <naimaabrarshami@gmail.com>
2021-11-17 00:41:11 +0300

    change password

--- dumpling-management-system/src/Backend/routes/routes.js
+++ dumpling-management-system/src/Backend/routes/routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import {findUsers,addUser,getSQ} from "../../Backend/controller/userController.js";
+import {findUsers,addUser,getSQ,changePassword} from "../../Backend/controller/userController.js";
 const route = express.Router();
 
 route.post("/api/userController/login",findUsers);
 route.post("/api/userController/createAccount",addUser);
 route.post("/api/userController/securityQuestions",getSQ);
+route.post("/api/userController/changePassword",changePassword);
 
 export default route;

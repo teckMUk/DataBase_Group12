19d7ee23926b98b4b83f0d6b190e9e49d182f8a8
esha-fatima <eshafatima2001@gmail.com>
2021-11-17 00:04:11 +0300

    added 3 APIs

--- dumpling-management-system/src/Services_API/api.js
+++ dumpling-management-system/src/Services_API/api.js
@@ -9,5 +9,31 @@ export const LogIn = async (Email, Password) => {
     return await axios.post(`${Url}/userController/login`, object)
 }
 
+export const createAccount = async (userName, accountType, currentPassword, emailAddress, securityQuestions, employeeName, dateOfBirth, phoneNumber, address, position, salary,bankAccountNumber) => {
+    const object2 = {"userName" : userName,
+    "accountType":accountType,
+    "currentPassword" : currentPassword,
+    "emailAddress" : emailAddress,
+    "securityQuestions" : securityQuestions,
+    "employeeName" : employeeName,
+    "dateOfBirth" : dateOfBirth,
+    "phoneNumber" : phoneNumber,
+    "address" : address,
+    "position" : position,
+    "salary" : salary,
+    "bankAccountNumber" : bankAccountNumber
+
+
+    }
+    return await axios.post(`${Url}/userController/createAccount`, object2)
+}
+
+export const securityQuestions = async (Email) => {
+    const object3 = {"email" : Email
+    }
+    return await axios.post(`${Url}/userController/securityQuestions`, object3)
+}
+
+
 
 

a61d22bb6d83d8c19afd9b51ba05c0ac5b04cc22
esha-fatima <eshafatima2001@gmail.com>
2021-11-18 01:41:22 +0300

    API completion

--- dumpling-management-system/src/Backend/routes/routes.js
+++ dumpling-management-system/src/Backend/routes/routes.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import express from 'express';
-import {findUsers,addUser,getSQ,changePassword,forgetPassword,validateSecurity} from "../../Backend/controller/userController.js";
+import {findUsers,addUser,getSQ,changePassword,forgetPassword,validateSecurity, accountExistence} from "../../Backend/controller/userController.js";
 dotenv.config({path:"./src/Backend/.env"});
 const route = express.Router();
 route.post("/api/userController/login",findUsers);

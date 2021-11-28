4377791c0b9aaa8430be61eac773d29b5349208e
MukRaza <Mukeet@tcig.co>
2021-11-28 11:05:50 +0500

    fix a warning

--- dumpling-management-system/src/Backend/controller/chefController.js
+++ dumpling-management-system/src/Backend/controller/chefController.js
@@ -2,7 +2,6 @@ import dotenv from "dotenv";
 import mysql from 'mysql';
 import express from 'express';
 import bodyParser from "body-parser";
-import sha1 from 'sha1';
 dotenv.config({path:"./src/Backend/.env"});
 const app = express();
 app.use(bodyParser.json({ extended: true }));

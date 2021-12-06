7a24e6a4f9179a5ac52503fad60d7c03db941ac4
esha-fatima <eshafatima2001@gmail.com>
2021-12-06 17:37:38 +0300

    safe

--- dumpling-management-system/src/Backend/routes/routes.js
+++ dumpling-management-system/src/Backend/routes/routes.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import express from 'express';
 import {findUsers,addUser,getSQ,changePassword,forgetPassword,validateSecurity, accountExistence, updateAccount, deleteAccount,getEmployeeDetails} from "../../Backend/controller/userController.js";
 import {addMenuItem,removeMenuItem,fetchDishIds,viewPlacedOrders,changeOrderStatus} from "../../Backend/controller/chefController.js";
-import {updateEmployeeSalary,fetchAllEmployee, giveBonuses} from "../../Backend/controller/managerController.js";
+import {updateEmployeeSalary,fetchAllEmployee, giveBonuses, applyCoupon,addCoupon } from "../../Backend/controller/managerController.js";
 import {placeOrder,viewOrderSummary,dailySaleReport} from "../../Backend/controller/cashierController.js";
 dotenv.config({path:"./src/Backend/.env"});
 const route = express.Router();

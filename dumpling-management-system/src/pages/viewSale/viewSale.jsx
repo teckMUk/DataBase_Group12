8714997e30c9cb8f8de782d0c626f3a1fc85fa61
Emaan <23100276@lums.edu.pk>
2021-12-11 17:13:24 +0500

    drop added

--- dumpling-management-system/src/pages/viewSale/viewSale.jsx
+++ dumpling-management-system/src/pages/viewSale/viewSale.jsx
@@ -2,42 +2,39 @@ import React from 'react';
 import SaleTable from '../../components/saleTable/saleTable';
 import Nav2 from '../../components/nav/nav';
 import { useState, useEffect } from "react";
+import { Dropdown} from 'react-bootstrap';
 
 const initialState = {
     saleType: ""
 };
 export default function Sale()
 {
-    
     const [newEmploye, setNewEmploye] = useState(initialState);
     useEffect(() => {}, [newEmploye]);
     let check = 0;
 
     const handle = (e)=>
     {   
-        console.log('xyz', e.target);
-        const value1 = e.target.value;
-        console.log("val is",value1);
-        
         const {name, value} = e.target;
-        console.log(name);
-        console.log('name is:',name);
-        console.log('val is:',value);
+        // console.log(name);
+        // console.log('name is:',name);
+        // console.log('val is:',value);
         
         setNewEmploye({...newEmploye, [name]: value});
-        
+    }   
+    const checkHandle = () => {
         if(newEmploye.saleType=== "yearly")
         {
-            console.log('check 0 ', check);
-            check = 0;
+            check = 1;
+            console.log('check 1', check);
         }
         else if(newEmploye.saleType === "monthly")
         {
-            console.log('check 1 ', check);
-            check = 1;
-        }
-        
-    }   
+            check = 2;
+            console.log('check 2', check);
+        }  
+        return check;
+    }
   
     return (
         <>
@@ -50,13 +47,29 @@ export default function Sale()
                     <h1>Select The Sale Type</h1>
                     <p><input type="radio" name = "saleType" value="yearly" />Yearly Sales</p>
                     <p><input type="radio" name = "saleType" value="monthly" />Monthly Sales</p>
-                   {console.log("hello")}
                 </div>
-        
 
                 <div>
-                    <SaleTable/>
+                <Dropdown>
+                    <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+                    Dropdown Button
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu variant="dark">
+                    <Dropdown.Item href="#/action-1" active>
+                        Action
+                    </Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item href="#/action-4">Separated link</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
                 </div>
+
+                {(checkHandle()=== 2) && <div>
+                    <SaleTable/></div>}
+                
             </div>
         </>
     )
